d2bd7dff89dff84d423166ff05100fc9c9795b13
Fred Picard <50530374+FredPicard@users.noreply.github.com>
2019-10-29 10:29:05 -0700

    added boolean loops
    added a boolean comparison while and do/while loops

---
+++ booleanWhile.cs/booleanWhile.cs/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace booleanWhile.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 1;
+            while (x <= 20)
+            {
+                Console.WriteLine(x);
+                x++;
+            }
+            Console.ReadLine();
+        }
+    }
+}
