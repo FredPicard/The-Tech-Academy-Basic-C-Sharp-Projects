cefbc83215773e4ad7da630945e6e691a12b037f
Fred Picard <50530374+FredPicard@users.noreply.github.com>
2019-11-04 11:39:35 -0800

    added Exception Handling
    added basic exception handling and try catch examble for my own rerefence

---
+++ ExceptionHandling.cs/ExceptionHandling.cs/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace ExceptionHandling.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Pick a number.");
+                int numberOne = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Pick a second number");
+                int numberTwo = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Dividing the two...");
+                int numberThree = numberOne / numberTwo;
+                Console.WriteLine(numberOne + " divided by " + numberTwo + " equals " + numberThree);
+                Console.ReadLine();
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine(ex.Message);//(or i could type "Please enter a whole number whithin the parantheses//
+                return;
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine("Please don't divide by zero");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+          
+        }
+        
+    }
+}
