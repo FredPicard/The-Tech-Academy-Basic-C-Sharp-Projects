c2126789315da4fa134699edd454eb2e3041d151
Fred Picard <50530374+FredPicard@users.noreply.github.com>
2019-11-04 18:15:20 -0800

    Added steps 9 - 11
    added redone steps 9-11

---
+++ iterationExercise2.cs/iterationExercise2.cs/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace iterationExercise2.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //#9 & 10
+            Console.WriteLine("Please type in Squash, Cucumbers, Onions, Sprouts, or Lettuce");
+            List<string> veggies = new List<string> { "Squash", "Cucumbers", "Onions", "Sprouts", "Squash", "Lettuce" };
+            string veg = (Console.ReadLine());
+
+
+            bool found = false;
+            for (int i = 0; i < veggies.Count; i++)
+            {
+                if (veggies[i] == veg)
+                {
+                    found = true;
+                    Console.WriteLine(i);
+                }
+
+            }
+            if (found == false)
+            {
+                Console.WriteLine("Not a valid entry");
+                
+            }
+         
+        
+      
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        //#11
+        String[] colors = new String[] { "red", "blue", "green", "red", "purple", "yellow", "red", "blue" };
+        //String[] array = new String[colors.Length];
+        List<String> distinct = new List<string>();
+                    foreach (string colour in colors)
+                    {
+                        Console.Write(colour);
+
+                        if (distinct.Contains(colour))
+                        {
+                            Console.Write($" <-- This color is repeated");
+                        }
+                        else
+                        {
+                            distinct.Add(colour);
+                        }
+                
+                        Console.WriteLine();
+
+
+
+                    }
+                    Console.ReadLine();
+
+                }
+            }
+        }
--- iterationExercise2.cs/iterationExercise2.cs/Properties/AssemblyInfo.cs
+++ iterationExercise2.cs/iterationExercise2.cs/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("iterationExercise.cs")]
+[assembly: AssemblyTitle("iterationExercise2.cs")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("iterationExercise.cs")]
+[assembly: AssemblyProduct("iterationExercise2.cs")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("8e3dffbb-cfd4-4d6d-8f58-6b2595317b25")]
+[assembly: Guid("3d1955fc-f105-45ad-9604-09b106b14f2f")]
 
 // Version information for an assembly consists of the following four values:
 //
