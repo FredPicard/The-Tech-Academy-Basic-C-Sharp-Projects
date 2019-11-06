f725ee862bf424da50ccf5c97258ea3dbd3389b2
Fred Picard <50530374+FredPicard@users.noreply.github.com>
2019-11-06 12:30:45 -0800

    Added class exercise
    added an exercise that instantiated a class, initiatlized a constructor

---
+++ classExcercise.cs/classExcercise.cs/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classExcercise.cs
+{
+    class Program
+    {
+        private static void Main(string[] args)
+
+        {
+            Calculator Number = new Calculator();
+
+            int num1 = 2;
+            Console.WriteLine("Please enter a number");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine( Number.Add(num1, num2) );
+            Console.WriteLine(Number.Multiply(num1, num2));
+            Console.WriteLine(Number.Subtract(num1, num2));
+            Console.ReadLine();
+           
+        }
+    
+    }
+}
---
+++ classExcercise.cs/classExcercise.cs/class1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classExcercise.cs
+{
+     class Calculator
+    {
+
+        public Calculator()
+        {
+
+
+        }
+
+            public int Add(int num1, int num2)
+
+            {
+
+                return (num1 + num2);
+
+            }
+
+            public int Multiply(int num1, int num2)
+
+            {
+
+
+
+                return (num1 * num2);
+
+            }
+
+            public int Subtract(int num1, int num2)
+
+            {
+
+
+                return (num1 - num2);
+              
+            }
+          
+    }
+}
+
+
+
+
