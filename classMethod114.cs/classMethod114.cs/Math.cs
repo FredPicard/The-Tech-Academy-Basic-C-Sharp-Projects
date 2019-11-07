d9359499f69513487c0d84a871b89d0207f76243
Fred Picard <50530374+FredPicard@users.noreply.github.com>
2019-11-07 11:44:13 -0800

    Added 3 methods to  one class
    added 3 methods to one class

---
+++ classMethod114.cs/classMethod114.cs/Math.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classMethod114.cs
+{
+    class Math1
+    {
+     static  public void Number( int num1)
+        {
+            Console.WriteLine(num1 * 12);
+           
+        }
+          
+       static public void get( decimal num2)
+        {
+            Console.WriteLine( num2 / 5);
+        }
+
+        static public void Convert( int num3)
+        {
+            Console.WriteLine( num3 * 5 );
+        }
+
+
+
+
+
+
+
+    }
+}
---
+++ classMethod114.cs/classMethod114.cs/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classMethod114.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Math1 math = new Math1();
+
+            Math1.Number(num1: 12);
+
+            Math1.get(num2: 12);
+
+            string num3 = "100";
+            int value = Convert.ToInt32(num3);
+            Math1.Convert(value);
+            Console.ReadLine();
+                
+        }
+
+    }
+}
