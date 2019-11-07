f0f9364413aaae738907e18cffd657cb6454be1b
Fred Picard <50530374+FredPicard@users.noreply.github.com>
2019-11-07 10:04:07 -0800

    Added class method exercise
    Added mrthod in class, instantiation

---
+++ methodsClass.cs/methodsClass.cs/Calc.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace methodsClass.cs
+{
+    class Calc
+    {
+        static public void Cal(int num1, int num2)
+        {
+
+
+            num1 = (int)Math.Sqrt(num1);
+
+            Console.WriteLine(num2);
+
+           
+           
+
+        }
+        
+    }
+}
---
+++ methodsClass.cs/methodsClass.cs/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace methodsClass.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+          
+
+            Calc Number = new Calc();
+
+            Calc.Cal(num1: 4, num2: 5);
+
+            Console.ReadLine();
+
+
+
+        }
+
+        
+
+    }
+
+}
