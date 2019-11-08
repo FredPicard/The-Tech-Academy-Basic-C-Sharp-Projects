c3da5f1ca0db2c185ac6e22abf7fe6e095e9b1ea
Fred Picard <50530374+FredPicard@users.noreply.github.com>
2019-11-08 14:06:49 -0800

    Added optional parameter
    added optional parameter with an empty string data type

---
+++ classMethod115/classMethod115/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classMethod115
+{
+    class Class1
+    {
+        public int Number(int num1, int num2 = 10)
+        {
+            
+                return (num1 + num2);
+            
+        }
+    }
+}
---
+++ classMethod115/classMethod115/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classMethod115
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 Num = new Class1();
+          
+            Console.WriteLine("Please enter a number");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please enter another number if you like");
+            string response = Console.ReadLine();
+
+            if (response == "")
+
+            {
+                Console.WriteLine(Num.Number(num1));
+            }
+            else
+            {
+                int num2 = Convert.ToInt32(response);
+                Console.WriteLine(Num.Number(num1,num2));
+
+            }
+           
+            
+            Console.ReadLine();
+
+
+
+            
+
+        }
+    }
+}
