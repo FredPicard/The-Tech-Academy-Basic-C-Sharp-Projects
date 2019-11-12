2b28dd1cfd8c31afd1a36c577aeed4c4064e5f96
Fred Picard <50530374+FredPicard@users.noreply.github.com>
2019-11-12 10:34:30 -0800

    added abstract;/virtual method
    added abstract class  and virtual method

---
+++ abstractVirtual124/abstractVirtual124/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace abstractVirtual124
+{
+    class Employee : Person
+    {
+
+        public new void SayName()
+        {
+            throw new NotImplementedException();
+           
+        }
+
+
+    }
+}
