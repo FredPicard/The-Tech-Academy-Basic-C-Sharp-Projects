2f4555ce9a8de19b201ebb35623b862756c26ea9
Fred Picard <50530374+FredPicard@users.noreply.github.com>
2019-11-12 10:19:16 -0800

    added class method
    added class method

---
+++ classMethodEx117/classMethodEx117/Hello.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classMethodEx117
+{
+    class Hello
+    {
+
+        
+        public void Number(int num1, out int num2)
+        {
+
+            num2 = num1 / 2;
+
+            
+
+
+        }
+
+       
+        public void Number()
+        {
+
+
+          
+        }
+
+    }
+
+
+    public static class secondClass {
+    
+
+    }
+
+
+}
+
+
+
+
---
+++ classMethodEx117/classMethodEx117/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classMethodEx117
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hello Num = new Hello();
+
+            Console.WriteLine("Please Enter A Number");
+            int input = Convert.ToInt32(Console.ReadLine());
+            int output;
+            Num.Number(input , out output);
+            Console.WriteLine(output);
+
+
+            Console.ReadLine();
+
+        }
+          
+
+    }
+}
