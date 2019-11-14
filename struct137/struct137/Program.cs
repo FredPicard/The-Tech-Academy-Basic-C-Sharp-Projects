56dc29b1875594d6733755cba4c0ebf2dffc855c
Fred Picard <50530374+FredPicard@users.noreply.github.com>
2019-11-14 11:36:40 -0800

    Added struct
    Added struct with a decimal property

---
+++ struct137/struct137/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace struct137
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number number;
+            number.Amount = 10;
+
+
+            Console.WriteLine(number.Amount);
+            Console.ReadLine();
+        }
+    }
+}
