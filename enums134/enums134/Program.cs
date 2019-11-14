3c9c2c234ca304e47e5f840cfcb439e6a4593437
Fred Picard <50530374+FredPicard@users.noreply.github.com>
2019-11-14 10:19:36 -0800

    Added enum
    Added an enum with a try/catch

---
+++ enums134/enums134/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace enums134
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Week week = new Week();
+          
+            Console.WriteLine("Please enter a day of the week");
+            string day = (Console.ReadLine());
+            Console.WriteLine(day);
+
+            DaysOfTheWeek days;
+            try
+            {
+                days = (DaysOfTheWeek)Enum.Parse(typeof(DaysOfTheWeek), day);
+           
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Please enter an actual day of the week");
+
+                
+            }
+
+           
+
+            Console.ReadLine();
+                
+        }
+    }
+}
---
+++ enums134/enums134/Week.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace enums134
+{
+    class Week
+    {
+        public DaysOfTheWeek Day { get; set; }
+        }
+    }
+public enum DaysOfTheWeek
+{
+    Monday,
+    Tuesday,
+    Wednesday,
+    Thursday,
+    Friday,
+    Saturday,
+    Sunday,
+    None
+}
