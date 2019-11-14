6fa9904eed4c46de1a4423627770403d718dd8b6
Fred Picard <50530374+FredPicard@users.noreply.github.com>
2019-11-13 16:21:38 -0800

    Added generic property
    added generic property to string and interger values

---
+++ generic131/generic131/Employee.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace generic131
+{
+    class Employee<T>
+        
+    {
+
+
+        public List <T> Things { get; set; }
+       
+       
+      
+        
+        
+ 
+       
+}
+
+}
+
---
+++ generic131/generic131/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace generic131
+{
+    class Program 
+    {
+        static void Main(string[] args)
+        {
+            
+            Employee<string> employee1 = new Employee<string>()  ;
+            employee1.Things = new List<string> {"Bob", "Betty", "Billy" };
+          
+            foreach (string name in employee1.Things)
+              Console.WriteLine(name);
+
+
+
+
+            Employee<int> employee = new Employee<int>() ;
+            employee.Things = new List<int> { 1, 2, 3 };
+
+            foreach (int number in employee.Things )
+                Console.WriteLine(number);
+         
+            
+           
+            Console.ReadLine();
+        }
+       
+    }
+}
