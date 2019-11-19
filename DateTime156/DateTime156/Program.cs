68848a61cd7aa33da5e5d61e8790bf3c79e634a4
Fred Picard <50530374+FredPicard@users.noreply.github.com>
2019-11-19 11:04:46 -0800

    added datetime
    added dateTime to users entry of time

---
+++ DateTime156/DateTime156/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateTime156
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DateTime.Now);
+            Console.WriteLine("Please enter a number ");
+           int num = Convert.ToInt32(Console.ReadLine());
+           var combinedTime = DateTime.Now. AddHours(num);
+           Console.WriteLine(combinedTime);
+            Console.ReadLine();
+        } 
+    }
+}
