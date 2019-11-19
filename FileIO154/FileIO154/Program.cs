7b75a144d0c908852140260a5e2c5f950e94d8a7
Fred Picard <50530374+FredPicard@users.noreply.github.com>
2019-11-19 11:46:30 -0800

    Added print content of logging text file
    added the ability to print the contents of the text file

--- FileIO154/FileIO154/Program.cs
+++ FileIO154/FileIO154/Program.cs
@@ -15,21 +15,31 @@ namespace FileIO154
 
 
            
-            string file1 = "C:\\Users\\Student\\Logs\\log.txt";
+           string file1 = "C:\\Users\\Student\\Logs\\log.txt";
             Console.WriteLine("please enter a number");
             string num = Console.ReadLine();
             using (StreamWriter file = new StreamWriter("C:\\Users\\Student\\Logs\\log.txt", true))
             {
                 file.WriteLine(num);
-                Console.WriteLine(file1);
+            }
+
+           
+
+            using (StreamReader sr = File.OpenText(file1))
+            {
+                string s;
+                while ((s = sr.ReadLine()) !=null)
+                {
+                    Console.Out.WriteLine(s);
+                }
+              
             }
 
 
-              
-            
 
 
-            Console.ReadLine();
+
+                Console.ReadLine();
             }
 
         } 
