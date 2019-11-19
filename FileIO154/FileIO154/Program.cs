a1706857389143317a398b2a508eb8efdeb3e294
Fred Picard <50530374+FredPicard@users.noreply.github.com>
2019-11-18 16:48:52 -0800

    added log in to file
    qdded a log in from user to file

---
+++ FileIO154/FileIO154/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace FileIO154
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+           
+            string file1 = "C:\\Users\\Student\\Logs\\log.txt";
+            Console.WriteLine("please enter a number");
+            string num = Console.ReadLine();
+            using (StreamWriter file = new StreamWriter("C:\\Users\\Student\\Logs\\log.txt", true))
+            {
+                file.WriteLine(num);
+                Console.WriteLine(file1);
+            }
+
+
+              
+            
+
+
+            Console.ReadLine();
+            }
+
+        } 
+    }
