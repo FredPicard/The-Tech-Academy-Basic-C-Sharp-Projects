424a74863bae321bcb9b0128706e01eca90aac12
Fred Picard <50530374+FredPicard@users.noreply.github.com>
2019-11-19 16:11:21 -0800

    Added Const variable, "var" keyword, chain two constructors
    added const variable, var "keyword" and chain two constructors

---
+++ excercise162/excercise162/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace excercise162
+{
+   public class Person
+    {
+        public Person() : this(10)
+        {
+            
+        }
+
+        public Person(int Age)
+        {
+            Console.WriteLine(Age);
+        }
+    }
+}
---
+++ excercise162/excercise162/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace excercise162
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Person person = new Person();
+
+
+            //a const variable
+            const int Months = 12;
+            Console.WriteLine(Months);
+
+
+            ////variable using key word "var"
+            var age = 25;
+            Console.WriteLine(age);
+            Console.ReadLine();
+        }
+
+
+      
+    }
+
+       
+       
+   
+
+
+
+       
+        }
+      
+            
+       
+        
+      
+  
+
+        
+    
+
