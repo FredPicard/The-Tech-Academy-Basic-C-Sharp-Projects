d6af8ab3e8c6373c88fdfaff4835bab6a1af08c9
Fred Picard <50530374+FredPicard@users.noreply.github.com>
2019-12-02 14:24:35 -0800

    Added try catch
    Added try catch

---
+++ tryCatchExceptions165/tryCatchExceptions165/Program.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tryCatchExceptions165
+{
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+
+
+
+            try 
+            {
+
+                //AgeException zeroage = new AgeException("NO zero");
+                //AgeException negage = new AgeException("No negative numbers");
+
+
+                Console.WriteLine("Please enter your age");
+                int age = Convert.ToInt32(Console.ReadLine());
+
+                if (age == 0)
+                {
+                    throw new AgeException("NO zeros");
+                }
+                if (age < 0)
+                {
+                    throw new AgeException("NO negative numbers");
+                }
+
+                int dob = 2019 - age;
+                Console.WriteLine(dob);
+                }
+
+                catch(AgeException a)
+                {
+                    Console.WriteLine(a.Message);
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Thats not a number");
+                }
+            
+                  
+          
+                
+                
+            
+
+
+              
+
+
+                Console.ReadLine();
+
+
+
+
+            }
+
+            }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
